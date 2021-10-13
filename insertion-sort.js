0268c745940cf3f293d94f3756d99931fdfd9905
naisannovel <naisannovel@gmail.com>
2021-10-13 21:47:10 +0600

    update

--- insertion-sort.js
+++ insertion-sort.js
@@ -1,18 +1,18 @@
-const result = [54, 33, 31, 25, 26, 85, 98, 15, 9, 85, 99, 45, 12, 75, 79, 4, 88, 42, 61];
+const result = [33, 54, 31, 25, 85, 98, 15, 9, 85, 99, 45, 12, 75, 79, 4, 88, 42, 61];
 
 function insertion_sort(ary){
 
-    for(i=0;i <= ary.length - 1;i++){
+    for(i = 1; i <= ary.length-1; i++){
         
-    const item = ary[i];
-    let j = i - 1;
+        const item = ary[i];
+        let j = i - 1;
 
-    while(j >= 0 && ary[j] > item){
-        ary[j+1] = ary[j];
-        j = j - 1;
-    }
+        while(j >= 0 && ary[j] > item){
+            ary[j+1] = ary[j];
+            j = j - 1;
+        }
 
-    ary[j + 1] = item;
+        ary[j+1] = item;
 
     }
     return ary;

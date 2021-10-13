410ef7bf88b28c75fbaaef275add84445fd8d707
naisannovel <naisannovel@gmail.com>
2021-10-14 00:37:22 +0600

    update

--- stack.js
+++ stack.js
@@ -11,3 +11,15 @@ push(ary,5)
 push(ary,5)
 push(ary,5)
 console.log(ary);
+
+function pop(ary){
+    if(ary.length){
+        const item = ary[ary.length-1];
+        return item;
+    }else{
+        console.log('stack is empty');
+    }
+}
+
+const r = pop(ary);
+console.log(r);

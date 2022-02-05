96da8355f197f9151a9ec1a9f42f1f9d4d83bc34
naisannovel <naisannovel@gmail.com>
2022-02-06 01:31:20 +0600

    update

--- search/binary-search.js
+++ search/binary-search.js
@@ -8,53 +8,28 @@ const roll = [1, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 6
 // 2*2*2*2*2 = 32
 // 30 middle within 16 & 32 so 5 times
 
+// Condition -> if data have sorted then we can use binary search
 
-// function binary_search(ary, num){
-//     let start = 0;
-//     let end = ary.length - 1;
-
-//     while(start <= end){
-//         const middle = Math.round((start + end) / 2);
-
-//         if(ary[middle] === num){
-//             return 'found';
-//         }
-
-//         if(ary[middle] > num){
-//             end = middle - 1;
-//         }else{
-//             start = middle + 1;
-//         }
-//     }
-
-//     return 'not found';
-// }
-
-// const result = binary_search(roll, 46);
-// console.log(result);
-
-
-function binary_search(ary,x){
+function binary_search(ary, num){
     let start = 0;
     let end = ary.length - 1;
 
-
     while(start <= end){
-console.log('running...');
-        const middle = Math.round(( start + end ) / 2);
+        const middle = Math.round((start + end) / 2);
 
-        if(ary[middle] === x){
-            return true;
+        if(ary[middle] === num){
+            return 'found';
         }
 
-        if(middle > x){
+        if(ary[middle] > num){
             end = middle - 1;
         }else{
             start = middle + 1;
         }
-
     }
 
-    return false;
-
+    return 'not found';
 }
+
+const result = binary_search(roll, 46);
+console.log(result);

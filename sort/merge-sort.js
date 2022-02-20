2996e6de01d3053ba27eabd96d480d5048465f3f
naisannovel <naisannovel@gmail.com>
2022-02-20 22:06:08 +0600

    update

--- sort/merge-sort.js
+++ sort/merge-sort.js
@@ -1,14 +1,40 @@
 
-// function merge_sort(start,end){
-//     // const start = 0;
-//     // const end = ary.length - 1;
+const roll = [8,1,4,5,7,6,3,2];
 
-//     // if(start >= end){
-//     //     return;
-//     // }
+function merge(left, right){
+    let sorted = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
 
-//     const middle = start + (end - start) / 2;
-//     console.log(middle);
-// }
+    while(leftIndex < left.length && rightIndex < right.length){
 
-// merge_sort(0,12)
+        const leftEl = left[leftIndex];
+        const rightEl = right[rightIndex];
+
+        if (leftEl < rightEl){
+            sorted.push(leftEl);
+            leftIndex++;
+        }else{
+            sorted.push(rightEl);
+            rightIndex++;
+        }
+    }
+    return [...sorted, ...left.slice(leftIndex), ...right.slice(rightIndex)];
+}
+
+
+function merge_sort(ary){
+
+    if(ary.length <= 1){
+        return ary;
+    }
+
+    let mid = Math.floor(ary.length / 2);
+
+    const left = merge_sort(ary.slice(0, mid));
+    const right = merge_sort(ary.slice(mid));
+    return merge(left, right);
+}
+
+const res = merge_sort(roll)
+console.log(res);
